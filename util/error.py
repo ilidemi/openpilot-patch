46091799396619f3208db5fd58d975b71c423f20
Ilia Demianenko <belk94@gmail.com>
2021-05-17 13:17:32 -0700

    commit

--- util/error.py
+++ util/error.py
@@ -1,1 +1,10 @@
-print("Error from python")
+import time
+
+while True:
+    brightness_path = '/sys/class/leds/lcd-backlight/brightness'
+    with open(brightness_path, 'w') as brightness_f:
+        brightness_f.write('63')
+    time.sleep(1.0)
+    with open(brightness_path, 'w') as brightness_f:
+        brightness_f.write('127')
+    time.sleep(1.0)
