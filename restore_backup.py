da89728d00554d410e7dc241a6efe2533b43ff89
Ilia Demianenko <belk94@gmail.com>
2021-05-17 12:20:19 -0700

    fix

--- restore_backup.py
+++ restore_backup.py
@@ -6,9 +6,9 @@ backup_path = '/data/openpilot-patch/backup'
 
 for (parent, dirs, files) in os.walk(backup_path):
     for file in files:
-        rel_path = f'{parent}/{file}'
-        disk_full_path = f'{disk_path}/{rel_path}'
+        backup_full_path = f'{parent}/{file}'
+        disk_full_path = f'{disk_path}/{backup_full_path}'
         if os.path.exists(disk_full_path):
             os.remove(disk_full_path)
-        shutil.copy(f'{backup_path}/{rel_path}', disk_full_path)
-        print(f'{rel_path} restored')
+        shutil.copy(backup_full_path, disk_full_path)
+        print(f'{backup_full_path} restored')
