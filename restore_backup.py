737bc4ba6e673ec9d1f35a95a9c0c915bd9a39b6
Ilia Demianenko <belk94@gmail.com>
2021-05-17 12:19:24 -0700

    fix

--- restore_backup.py
+++ restore_backup.py
@@ -4,7 +4,7 @@ import shutil
 disk_path = '/data/openpilot/selfdrive/controls/lib'
 backup_path = '/data/openpilot-patch/backup'
 
-for (parent, dirs, files) in os.walk('backup'):
+for (parent, dirs, files) in os.walk(backup_path):
     for file in files:
         rel_path = f'{parent}/{file}'
         disk_full_path = f'{disk_path}/{rel_path}'
